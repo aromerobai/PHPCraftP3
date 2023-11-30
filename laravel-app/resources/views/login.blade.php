e82b45620c2a3e53a495a74cab4573fdf477c575
danielfreijo <146752570+danielfreijo@users.noreply.github.com>
2023-11-30 09:56:01 +0100

    cambios

--- laravel-app/resources/views/login.blade.php
+++ laravel-app/resources/views/login.blade.php
@@ -1,16 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Login</h2>
-    @foreach ($usuarios as $usuario)
-            <li>{{ $gitusuario->Username }}</li>
-            <!-- Aquí puedes mostrar otros atributos del usuario -->
-    @endforeach
-</body>
-</html>
+<!-- login.blade.php -->
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="text-login">Log In</h1>
+
+                        @if (isset($mensaje))
+                            <p class="alert alert-info">{{ $mensaje }}</p>
+                        @endif
+
+                        <!-- Formulario de inicio de sesión -->
+                        <form action="{{ route('login') }}" method="post">
+                            @csrf <!-- Agrega el token CSRF para seguridad -->
+
+                            <!-- Campo de usuario -->
+                            <div class="form-group">
+                                <label for="usuario">Usuario:</label>
+                                <input type="text" id="usuario" name="usuario" class="form-control" required>
+                            </div>
+
+                            <!-- Campo de contraseña -->
+                            <div class="form-group">
+                                <label for="contrasena">Contraseña:</label>
+                                <input type="password" id="contrasena" name="contrasena" class="form-control" required>
+                            </div>
+
+                            <!-- Botón de envío del formulario -->
+                            <button type="submit" name="loginUsuario" class="btn btn-primary btn-block">Iniciar sesión</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
